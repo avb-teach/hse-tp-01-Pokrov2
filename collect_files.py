6658740413125284b83cf3d2b51494067222eb55
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-24 20:55:23 +0300

    pre_pre_last

--- collect_files.py
+++ collect_files.py
@@ -10,22 +10,29 @@ def do_copy(in_dir, out_dir, max_depth=None):
         os.makedirs(out_dir)
 
     names = {}
+
     for curr_root, folders, files in os.walk(in_dir):
         rel_path = os.path.relpath(curr_root, in_dir)
-        depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1
+        if rel_path == ".":
+            depth = 0
+        else:
+            rel_path.count(os.sep) + 1
 
         if max_depth is not None and depth > max_depth:
             folders[:] = []
             continue
+
+        if max_depth is not None and depth > max_depth:
+            continue
+
         for f in files:
-            if max_depth is not None and depth > max_depth:
-                continue
             orig_path = os.path.join(curr_root, f)
+
             if f in names:
                 names[f] += 1
                 dot = f.rfind(".")
                 if dot != -1:
-                    new_name = f[:dot] + f"_" + str(names[f]) + f[dot:]
+                    new_name = f[:dot] + "_" + str(names[f]) + f[dot:]
                 else:
                     new_name = f + "_" + str(names[f])
             else:
@@ -36,7 +43,6 @@ def do_copy(in_dir, out_dir, max_depth=None):
             shutil.copy2(orig_path, final_path)
 
 
-n = 0
 in_dir = sys.argv[1]
 out_dir = sys.argv[2]
 max_depth = None
