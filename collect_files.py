1934dde2906794e3f10a3c99327c58e1f1f69296
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-24 21:14:50 +0300

    pre_pre_pre_pre_pre_pre_pre_last

--- collect_files.py
+++ collect_files.py
@@ -5,43 +5,41 @@ import shutil
 def do_copy(in_dir, out_dir, max_depth=None):
     if not os.path.exists(in_dir):
         sys.exit(1)
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    os.makedirs(out_dir, exist_ok=True)
 
     names = {}
-    for curr_root, folders, files in os.walk(in_dir):
-        rel_path = os.path.relpath(curr_root, in_dir)
-        if rel_path == ".":
-            depth = 0
-        else:
-            depth = rel_path.count(os.sep) + 1
 
-        if max_depth is not None and depth >= max_depth:
-            folders[:] = []
+    for root, dirs, files in os.walk(in_dir):
+        rel_path = os.path.relpath(root, in_dir)
+        depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1
 
         if max_depth is not None and depth > max_depth:
+            dirs[:] = []
             continue
 
-        for f in files:
-            orig_path = os.path.join(curr_root, f)
-            if f in names:
-                names[f] += 1
-                dot = f.rfind(".")
+        for name in files:
+            full_path = os.path.join(root, name)
+
+            if name in names:
+                names[name] += 1
+                dot = name.rfind(".")
                 if dot != -1:
-                    new_name = f[:dot] + "_" + str(names[f]) + f[dot:]
+                    new_name = name[:dot] + "_" + str(names[name]) + name[dot:]
                 else:
-                    new_name = f + "_" + str(names[f])
+                    new_name = name + "_" + str(names[name])
             else:
-                names[f] = 1
-                new_name = f
-            final_path = os.path.join(out_dir, new_name)
-            shutil.copy(orig_path, final_path)
+                names[name] = 1
+                new_name = name
+
+            dst_path = os.path.join(out_dir, new_name)
+            shutil.copy(full_path, dst_path)
 
 in_dir = sys.argv[1]
 out_dir = sys.argv[2]
 max_depth = None
+
 if "--max_depth" in sys.argv:
-    idx = sys.argv.index("--max_depth")
-    max_depth = int(sys.argv[idx + 1])
+    i = sys.argv.index("--max_depth")
+    max_depth = int(sys.argv[i + 1])
 
 do_copy(in_dir, out_dir, max_depth)
