e41f3ab58d99ec6471941b35e00ad6f59bd5c53a
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-24 20:45:54 +0300

    ttttt

--- collect_files.py
+++ collect_files.py
@@ -12,12 +12,13 @@ def do_copy(in_dir, out_dir, max_depth=None):
     for curr_root, folders, files in os.walk(in_dir):
         rel_path = os.path.relpath(curr_root, in_dir)
         depth = 0 if rel_path == "." else rel_path.count(os.sep) + 1
-
         if max_depth is not None and depth > max_depth:
             folders[:] = []
             continue
-
         for f in files:
+            if max_depth is not None and depth > max_depth:
+                continue
+
             orig_path = os.path.join(curr_root, f)
 
             if f in names:
@@ -30,6 +31,7 @@ def do_copy(in_dir, out_dir, max_depth=None):
             else:
                 names[f] = 1
                 new_name = f
+
             final_path = os.path.join(out_dir, new_name)
             shutil.copy2(orig_path, final_path)
 
@@ -37,7 +39,6 @@ def do_copy(in_dir, out_dir, max_depth=None):
 in_dir = sys.argv[1]
 out_dir = sys.argv[2]
 max_depth = None
-n = 0
 if "--max_depth" in sys.argv:
     idx = sys.argv.index("--max_depth")
     max_depth = int(sys.argv[idx + 1])
