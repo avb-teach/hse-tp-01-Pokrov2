dde1a3abb1c55d47c62cb182522ba05eb0cf4d1e
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-28 19:40:49 +0300

    done

--- collect_files.py
+++ collect_files.py
@@ -13,14 +13,15 @@ os.makedirs(output_directory, exist_ok=True)
 
 for curr_dir, folder, name in os.walk(input_directory):
     rel_path = os.path.relpath(curr_dir, input_directory)
-    if rel_path == '.':
+    if rel_path == "/":
         path_components = []
     else:
         path_components = rel_path.split(os.sep)
 
-    if rel_path != '.':
-        depth = len(path_components)  
-    else: depth = 0
+    if rel_path != "/":
+        depth = len(path_components)
+    else:
+        depth = 0
     if max_depth is not None and depth > max_depth:
         folder.clear()
         continue
@@ -31,17 +32,18 @@ for curr_dir, folder, name in os.walk(input_directory):
         list_of_folders = path_components
     if list_of_folders:
         short_path = os.path.join(*list_of_folders)
-    else:short_path = "."
+    else:
+        short_path = "/"
     last_folder = os.path.join(output_directory, short_path)
     os.makedirs(last_folder, exist_ok=True)
     for filename in sorted(name):
         src = os.path.join(curr_dir, filename)
         base, ext = os.path.splitext(filename)
         dst = os.path.join(last_folder, filename)
-        
+
         counter = 1
         while os.path.exists(dst):
             dst = os.path.join(last_folder, f"{base}_{counter}{ext}")
             counter += 1
-        
+
         shutil.copy2(src, dst)
