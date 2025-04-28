ada527f5f993b34e67cefae3ba2187646aa60944
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-28 19:51:23 +0300

    done

--- collect_files.py
+++ collect_files.py
@@ -2,40 +2,45 @@ import os
 import sys
 import shutil
 
-input_directory = sys.argv[1]
-output_directory = sys.argv[2]
-
+args = sys.argv[1:]
+input_directory = args[0]
+output_directory = args[1]
 max_depth = None
-if len(sys.argv) > 3:
-    max_depth = int(sys.argv[4])
+
+if "--max_depth" in args:
+    max_depth_index = args.index("--max_depth") + 1
+    if max_depth_index < len(args):
+        max_depth = int(args[max_depth_index])
 
 os.makedirs(output_directory, exist_ok=True)
 
 for curr_dir, folder, name in os.walk(input_directory):
     rel_path = os.path.relpath(curr_dir, input_directory)
-    if rel_path == "/":
+    if rel_path == ".":
         path_components = []
     else:
         path_components = rel_path.split(os.sep)
 
-    if rel_path != "/":
-        depth = len(path_components)
-    else:
-        depth = 0
+    depth = len(path_components)
+
     if max_depth is not None and depth > max_depth:
         folder.clear()
         continue
+
     if max_depth is not None:
-        start_index = max(0, len(path_components) - max_depth)
+        start_index = max(0, depth - max_depth)
         list_of_folders = path_components[start_index:]
     else:
         list_of_folders = path_components
+
     if list_of_folders:
         short_path = os.path.join(*list_of_folders)
     else:
-        short_path = "/"
+        short_path = "."
     last_folder = os.path.join(output_directory, short_path)
+
     os.makedirs(last_folder, exist_ok=True)
+
     for filename in sorted(name):
         src = os.path.join(curr_dir, filename)
         base, ext = os.path.splitext(filename)
