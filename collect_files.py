fc81fee16e1110dbe474aec65a0b4dfe4f576682
Sergey Martynov <Sergey251222005@yandex.ru>
2025-04-28 19:34:07 +0300

    done

--- collect_files.py
+++ collect_files.py
@@ -5,50 +5,40 @@ import shutil
 input_directory = sys.argv[1]
 output_directory = sys.argv[2]
 
-# max_depth = 0
-# if len(sys.argv) >= 4:
-max_depth = int(sys.argv[3]) - 1
+max_depth = None
+if len(sys.argv) > 3:
+    max_depth = int(sys.argv[3])
 
 os.makedirs(output_directory, exist_ok=True)
 
 for curr_dir, folder, name in os.walk(input_directory):
     rel_path = os.path.relpath(curr_dir, input_directory)
 
-    if rel_path == ".":
-        depth = 0
-    else:
-        depth = rel_path.count(os.sep) + 1
-
-    if (max_depth is not None) and depth > max_depth:
+    path_components = rel_path.split(os.sep)
+    if rel_path != '.':
+        depth = len(path_components)  
+    else: depth = 0
+    if max_depth is not None and depth > max_depth:
         folder.clear()
         continue
-
-    list_of_folders = []
-    if max_depth is not None:
-        list_of_folders = rel_path.split(os.sep)[:max_depth]
+    if max_depth is not None and len(path_components) > max_depth:
+        start_index = len(path_components) - max_depth
+        list_of_folders = path_components[start_index:]
     else:
-        list_of_folders = rel_path.split(os.sep)
-
-    if len(list_of_folders) > 0:
+        list_of_folders = path_components
+    if list_of_folders:
         short_path = os.path.join(*list_of_folders)
-    else:
-        short_path = "."
-
-    last_folder = output_directory
-    if short_path != ".":
-        last_folder = os.path.join(output_directory, short_path)
-
+    else:short_path = "."
+    last_folder = os.path.join(output_directory, short_path)
     os.makedirs(last_folder, exist_ok=True)
-
     for filename in sorted(name):
-        path_to_prev_file = os.path.join(curr_dir, filename)
-        base_name, ext = os.path.splitext(filename)
-        target_file = os.path.join(last_folder, filename)
-
+        src = os.path.join(curr_dir, filename)
+        base, ext = os.path.splitext(filename)
+        dst = os.path.join(last_folder, filename)
+        
         counter = 1
-        while os.path.isfile(target_file):
-            new_name = f"{base_name}_{counter}{ext}"
-            target_file = os.path.join(last_folder, new_name)
+        while os.path.exists(dst):
+            dst = os.path.join(last_folder, f"{base}_{counter}{ext}")
             counter += 1
-
-        shutil.copy2(path_to_prev_file, target_file)
+        
+        shutil.copy2(src, dst)
